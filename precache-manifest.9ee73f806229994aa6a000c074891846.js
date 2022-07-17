8768a225a3d082d5387f1f4add452f828bcac441
vivek2502 <vivekgupta.mailme@gmail.com>
2022-07-17 15:27:12 +0530

    Updates

---
+++ precache-manifest.9ee73f806229994aa6a000c074891846.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719a858d3cc8d168b6602237a48858a5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "19e4614f221f6d2fe78e",
+    "url": "./static/css/2.23e8e0a8.chunk.css"
+  },
+  {
+    "revision": "dce6a6b856de0a5af653",
+    "url": "./static/css/main.f9cab1d4.chunk.css"
+  },
+  {
+    "revision": "19e4614f221f6d2fe78e",
+    "url": "./static/js/2.e5fbfd91.chunk.js"
+  },
+  {
+    "revision": "95627054563e91a31aac3cca4196c71f",
+    "url": "./static/js/2.e5fbfd91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce6a6b856de0a5af653",
+    "url": "./static/js/main.23ec5dff.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  },
+  {
+    "revision": "b1455663ebd72fea96575fae5f48d270",
+    "url": "./static/media/aap.b1455663.png"
+  },
+  {
+    "revision": "8d36bd5eed1a54aec38ab960e65b81dc",
+    "url": "./static/media/bjp.8d36bd5e.png"
+  },
+  {
+    "revision": "c884959576b767c589bd7aa322b09e73",
+    "url": "./static/media/chelsea.c8849595.jpeg"
+  },
+  {
+    "revision": "fad1e81244c69a97e1a262ab6833b5ac",
+    "url": "./static/media/fcb.fad1e812.png"
+  },
+  {
+    "revision": "6291282f243b07be7c9ac0f0468dc3b5",
+    "url": "./static/media/hazard.6291282f.jpg"
+  },
+  {
+    "revision": "67c12fed1e18039bc1513d6b2f820448",
+    "url": "./static/media/kejriwal.67c12fed.jpg"
+  },
+  {
+    "revision": "50bd03f898bb7129e2303e920005ea1c",
+    "url": "./static/media/mbappe.50bd03f8.jpg"
+  },
+  {
+    "revision": "87c94cadca309052bc269edce55feb67",
+    "url": "./static/media/messi.87c94cad.jpg"
+  },
+  {
+    "revision": "8cdde503657e2a1f6e81ff8ec0d636d7",
+    "url": "./static/media/modi.8cdde503.jpg"
+  },
+  {
+    "revision": "59403460f7a015352da19bf8e48db9f7",
+    "url": "./static/media/neymar.59403460.jpg"
+  },
+  {
+    "revision": "d99f779eaa72817d4e74f55d2e159166",
+    "url": "./static/media/priyanka.d99f779e.jpg"
+  },
+  {
+    "revision": "84013d8f4affdf0a71cdd14717b38968",
+    "url": "./static/media/psg.84013d8f.png"
+  },
+  {
+    "revision": "aaf920869b8e9428a537a0cef66aa8fc",
+    "url": "./static/media/ronaldo.aaf92086.jpg"
+  }
+]);
